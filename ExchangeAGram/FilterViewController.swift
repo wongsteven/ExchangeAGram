f42c53db13aa4af360e9f9f325be4a349d2fdc35
Steven W <mr.wong.steven@gmail.com>
2015-06-14 18:45:23 -0400

    second commit

---
+++ ExchangeAGram/FilterViewController.swift
@@ -0,0 +1,23 @@
+//
+//  FilterViewController.swift
+//  ExchangeAGram
+//
+//  Created by steven wong on 6/14/15.
+//  Copyright (c) 2015 steven.w. All rights reserved.
+//
+
+import UIKit
+
+class FilterViewController: UIViewController {
+    
+    var thisFeedItem: FeedItem!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
